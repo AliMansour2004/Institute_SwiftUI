8b147fcf201892d25697324f2954df7f0dc60d21
Ali Mansour <ali.a.mansour2004@gmail.com>
2025-12-12 15:22:30 +0200

    Admin add University View

--- Institute_SwiftUI/Views/Universities/AddUniversityView.swift
+++ Institute_SwiftUI/Views/Universities/AddUniversityView.swift
@@ -5,3 +5,54 @@
 //  Created by Ali Mansour on 10/12/2025.
 //
 
+import SwiftUI
+import SwiftData
+
+struct AddUniversityView: View {
+    let admin: Admin
+    @EnvironmentObject var session: AuthSession
+    @Environment(\.modelContext) var context: ModelContext
+    @Environment(\.dismiss) var dismiss
+    @StateObject private var vm = AddUniversityViewModel()
+    
+    var body: some View {
+        VStack{
+            Text("Add a University")
+                .font(.largeTitle)
+                .bold()
+                .padding(.bottom, 30)
+            
+            TextField("University name", text: $vm.name)
+                .textFieldStyle(.roundedBorder)
+            
+            if let error = vm.error{
+                Text(error)
+                    .foregroundColor(.red)
+                    .font(.footnote)
+            }
+
+            Button{
+                vm.AddUniversity(context: context)
+                if vm.error == nil{
+                    dismiss()
+                }
+            }label: {
+                if vm.isLoading{
+                    ProgressView()
+                }else{
+                    Text("Add University")
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+            
+            Spacer()
+        } //vstack
+        .padding()
+    }
+}
+
+#Preview {
+    let admin = Admin()
+    AddUniversityView(admin: admin)
+}
